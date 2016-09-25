33a8a49b6c81d85cd0d6faf005ad349cf5e31118
PJ Legendre <pjlegendreiv@gmail.com>
2016-09-25 11:42:27 -0400

    Initial 0

---
+++ Assets/Plugins/UnityInteractionDesignFramework/Editor/InteractionObjectBuilder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class InteractionObjectBuilder : Editor {
+
+	[MenuItem("GameObject/Interaction/Object", priority = 10)]
+	[MenuItem("Assets/Create/Interaction/Object")]
+	public static void CreateInteractionObject() {
+
+	}
+
+	[MenuItem("GameObject/Interaction/One Click Button", priority = 10)]
+	[MenuItem("Assets/Create/Interaction/One Click Button")]
+	public static void CreateInteractionOneClickButton() {
+
+	}
+}
